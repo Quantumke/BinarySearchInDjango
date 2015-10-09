2438c94a4df911ecbb18b5ec7cf2dbaa08bdad28
Benson Mburu <nguruben@gmail.com>
2015-10-09 09:21:57 +0300

    created views for search

--- binarysearch/views.py
+++ binarysearch/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from myapp.forms import MyModelSearchForm
+
+def search(request):
+
+    if request.GET:
+        form = MyModelSearchForm(request.GET)
+        if form.is_valid():
+            results = form.get_result_queryset()
+        else:
+            results = []
+    else:
+        form = MyModelSearchForm()
+        results = []
+
+
+    return render_to_response(
+        'search.html',
+        RequestContext(request, {
+            'form': form,
+            'results': results,
+        })
+    )
